7f3153aa85add6b8f999d70122e5184a2756b9e8
papa <1605526341@qq.com>
2017-02-25 10:42:43 +0800

    this is my first use git

---
+++ myfir/hello.js
@@ -0,0 +1,14 @@
+function add(a,b){
+	a = a || 5;
+	b = b || 6;
+	return a+b;
+}
+
+function minus(a,b){
+	a = a || 16;
+	b = b || 10;
+	return a-b;
+}
+console.log(add(12,44));
+console.log(minus(5,9));
+
